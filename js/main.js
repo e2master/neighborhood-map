7193cf0e5da150fcdc43194bf9db76637d507915
e2master <271797772@qq.com>
2017-09-27 16:20:04 +0800

    '增加地图显示'

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// 百度地图API功能
+var map = new BMap.Map("map");
+map.centerAndZoom(new BMap.Point(116.404, 39.915), 14
+);
+// 启用滚轮放大缩小，默认禁用
+map.enableScrollWheelZoom();
+// 启用地图惯性拖拽，默认禁用
+map.enableContinuousZoom();
+// 添加默认缩放平移控件
+map.addControl(new BMap.ScaleControl({
+	anchor: BMAP_ANCHOR_TOP_LEFT
+}));
+map.addControl(new BMap.NavigationControl());
+
+// 获得地点的类型对象
+var getPlaceType = function(place) {
+	for(var i = 0, len = placeTypes.length; i < len; i++) {
+		if(placeTypes[i].type === place.type) {
+			return placeTypes[i];
+		}
+	}
+};
+// 为places增加地图标记
+var initPlacesMarker = function() {
+	for(var i = 0, len = places.length; i < len; i++) {
+		var place = places[i];
+		var myIcon = new BMap.Icon(getPlaceType(place).markerIcon, new BMap.Size(32, 32), {
+			anchor: new BMap.Size(16, 32)
+		});
+		var point = new BMap.Point(place.lng, place.lat);
+		places[i].marker = new BMap.Marker(point, {
+			icon: myIcon
+		});
+		map.addOverlay(places[i].marker);
+		
+	};
+};
+
+var showMarkers = function(places) {
+	map.clearOverlays();
+	for(var i = 0, len = places.length; i < len; i++) {
+		map.addOverlay(places[i].marker);
+	}
+}
+// 如果标记不在视图中，地图重新定位
+var moveCenter = function(place) {
+	
+	// 获取地图可视区域经纬度
+	var bs = map.getBounds(); //获取可视区域
+	var lb = bs.getSouthWest(); // 可视区域左下角
+	var rt = bs.getNorthEast(); // 可视区域右上角
+	
+	var topLat = rt.lat;
+	var bottomLat = lb.lat;
+	var leftLng = lb.lng;
+	var rightLng = rt.lng;
+	// 如果经纬度不在可视区域范围，重新定位到新的坐标
+	if ((place.lat < bottomLat || place.lat > topLat) || (place.lng < leftLng || place.lng > rightLng)) {
+		map.setCenter(new BMap.Point(place.lng, place.lat));
+	}
+}
+
+var showMarkerInfo = function(place) {
+	// 跳动的动画，2秒后停止
+	place.marker.setAnimation(BMAP_ANIMATION_BOUNCE);
+	moveCenter(place);
+	setTimeout(function(){
+		place.marker.setAnimation(null);
+	}, 2000);
+}
+
+initPlacesMarker();
+
+
--- js/placeViewModel.js
+++ js/placeViewModel.js
@@ -1,50 +1,50 @@
 var placeViewModel = function() {
 	// Data
 	var self = this;
+	// 地点类型
+	self.placeTypes = placeTypes;
 	// 控制查询界面是否显示，默认显示
 	self.controlShow = ko.observable(true);
 	// 查询条件
 	self.keyValue = ko.observable();
 	// 选择的类型
-	self.typeSelectes = ko.observableArray();
-	
-	self.currentProfit = ko.observable(0);
-	
-	// 不同类型的地图图标
-	self.placeMarkerIcon = [];
-	self.placeMarkerIcon['park'] = './img/park.png';
-	self.placeMarkerIcon['hotel'] = './img/hotel.png';
-	self.placeMarkerIcon['restaurant'] = './img/restaurant.png';
-	self.placeMarkerIcon['gymnasium'] = './img/gymnasium.png';
-	
-	// 地点类型
-	self.placeTypes = [
-		{type: 'park', markerIcon: './img/park.png', img: './img/公园.png'},
-		{type: 'hotel', markerIcon: './img/hotel.png', img: './img/饭店.png'},
-		{type: 'restaurant', markerIcon: './img/restaurant.png', img: './img/宾馆居住.png'},
-		{type: 'gymnasium', markerIcon: './img/gymnasium.png', img: './img/体育场馆.png'}
-	];
-	
-	self.ok = ko.pureComputed(function() {
-		return true;
+	self.typeSelectes = ko.observableArray(self.placeTypes);
+	// 地点列表
+	self.myPlaces = ko.pureComputed(function() {
+		var currentPlaces = places.filter(function(item){
+			if (self.keyValue()) {
+				return (item.name.indexOf(self.keyValue()) > -1) || (item.addr.indexOf(self.keyValue()) > -1); 
+			}
+			else {
+				return true;
+			}
+		}).filter(function(item) {
+			var result = false;
+			for (var i = 0, len = self.typeSelectes().length; i < len; i++) {
+				if (self.typeSelectes()[i].type === item.type) {
+					result = true;
+					break;
+				}
+			}
+			return result;
+		});
+		// 显示标记
+		showMarkers(currentPlaces);
+		return currentPlaces;
 	});
 	
+	// 用户当前选择类型
 	self.typeSelected = ko.pureComputed(function(data) {
 		var has = false;
 		for (var i = 0, len = self.typeSelectes().length; i < len; i++) {
 			if (self.typeSelectes[i] === data) {
 				has = true;
+				break;
 			}
 		}
 		return has;
 	});
 	
-	self.showAlert = function(data) {
-		window.alert(data)
-	};
-	
-
-
 	// 类型选择点击时维护用户选择的类型
 	self.typeClick = function(data) {
 		//如果没有选择添加，如果已经选择删除
@@ -53,15 +53,21 @@ var placeViewModel = function() {
 			if (self.typeSelectes()[i] === data) {
 				self.typeSelectes.splice(i, 1);
 				has = true;
+				break;
 			}
 		}
 		if (!has) {
 			self.typeSelectes.push(data);
 		}
 	};
-	
+	// 左侧列表显示控制
 	self.toggleControl = function() {
 		self.controlShow(!self.controlShow());
 	};
+	
+	// 列表点击对应标记跳动并显示信息窗口
+	self.placeClick = function(data) {
+		showMarkerInfo(data);
+	}
 };
 ko.applyBindings(new placeViewModel());
